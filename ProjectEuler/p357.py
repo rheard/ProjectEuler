68a1ec8cc2272dc4ed71cdf1344a4ce2090d00e8
Ryan Heard <ryanwheard@gmail.com>
2025-07-05 15:56:35 -0400

    Fix some personal inspection items

--- ProjectEuler/p357.py
+++ ProjectEuler/p357.py
@@ -2,27 +2,27 @@ from __future__ import print_function
 from sympy import divisors, isprime
 from math import sqrt
 
-"""
-We can start by reducing our search space. We can start this by using
-    the example of 30. Lets start with the divisor 1:
-
-    1 + 30/1 = 1 + 30 = 31
-
-    Great, but now if we do 30 we see
-
-    30 + 30/30 = 30 + 1
-
-    So we don't actually have to test all the divisors, we only have
-    to test divisors up to (and including) the sqrt(n).
-
-That will limit our d-space, but we can also reduce our n-space. We know
-    that every number must have itself (n) and 1 as divisors. By plugging
-    n in for d we get that n + n/n must be prime, or that n + 1 must be prime.
-    A consequence of this is that the only prime that will be included is
-    the prime where n + 1 is a prime, so 2.
-"""
-
 def solve(max_p=10**8):
+    """
+    We can start by reducing our search space. We can start this by using
+        the example of 30. Lets start with the divisor 1:
+
+        1 + 30/1 = 1 + 30 = 31
+
+        Great, but now if we do 30 we see
+
+        30 + 30/30 = 30 + 1
+
+        So we don't actually have to test all the divisors, we only have
+        to test divisors up to (and including) the sqrt(n).
+
+    That will limit our d-space, but we can also reduce our n-space. We know
+        that every number must have itself (n) and 1 as divisors. By plugging
+        n in for d we get that n + n/n must be prime, or that n + 1 must be prime.
+        A consequence of this is that the only prime that will be included is
+        the prime where n + 1 is a prime, so 2.
+    """
+
     running_sum = 0
     for n in range(2, max_p + 1, 4):
         if not isprime(n + 1):
